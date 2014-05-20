92fb2ee68da552deed54b472fab4dba0cae26ecf
Miłosz Sieradzki <milosz+github@sieradzki.it>
2014-05-20 22:23:22 +0200

    Add working redirection for Spring Framework documentation

--- redirectToUpToDateDocumentation.user.js
+++ redirectToUpToDateDocumentation.user.js
@@ -4,6 +4,7 @@
 // @description Detects if currently viewed online documentation page is the most recent version available, and if not, redirects user to the latest version.
 // @version     1.0.0
 // @match       *://docs.oracle.com/javase/*
+// @match       *://docs.spring.io/*
 // ==/UserScript==
 
 (function () {
@@ -17,7 +18,18 @@
 				return matches !== null && matches[2] !== this.currentVersion;
 			},
 			rewriteUrl: function (url) {
-				return url.replace(/\/javase\/([0-9\.]+)\/docs\/api\//, '/javase/'+ this.currentVersion + '/docs/api/');
+				return url.replace(/\/javase\/([0-9\.]+)\/docs\/api\//, '/javase/' + this.currentVersion + '/docs/api/');
+			}
+		},
+		springFramework: {
+			currentVersion: '4.0.5.RELEASE',
+			isOutdatedDocumentationPage: function (url) {
+				var matches = url.match(/^http(s)?:\/\/docs\.spring\.io\/spring\/docs\/([0-9\.A-Z]+)\/(javadoc\-)?api\//);
+				return matches !== null && matches[2] !== this.currentVersion;
+			},
+			rewriteUrl: function (url) {
+				return url.replace(/\/docs\/([0-9\.A-Z]+)\/(javadoc\-)?api\//, '/docs/' + this.currentVersion +
+					'/javadoc-api/');
 			}
 		}
 	};
