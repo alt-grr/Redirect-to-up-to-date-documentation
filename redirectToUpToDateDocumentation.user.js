8da71ab3858bfd9ba7fbe8b9a18a68cf59ad8b16
Miłosz Sieradzki <milosz+github@sieradzki.it>
2014-05-20 20:08:07 +0200

    Initial commit

---
+++ redirectToUpToDateDocumentation.user.js
@@ -0,0 +1,11 @@
+// ==UserScript==
+// @name        Redirect to up to date documentation
+// @namespace   sieradzki.it
+// @description Detects if currently viewed online documentation page is the most recent version available, and if not, redirects user to the latest version.
+// @version     1.0.0
+// @match       *
+// ==/UserScript==
+
+(function () {
+	'use strict';
+})();
