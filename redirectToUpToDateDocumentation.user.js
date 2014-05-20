80de2b39de422f8d98956ceb0b201162b7c7da9f
Miłosz Sieradzki <milosz+github@sieradzki.it>
2014-05-20 21:09:00 +0200

    Add boilerplate version of main userscript object

--- redirectToUpToDateDocumentation.user.js
+++ redirectToUpToDateDocumentation.user.js
@@ -3,9 +3,14 @@
 // @namespace   sieradzki.it
 // @description Detects if currently viewed online documentation page is the most recent version available, and if not, redirects user to the latest version.
 // @version     1.0.0
-// @match       *
+// @match       *://*/*
 // ==/UserScript==
 
 (function () {
 	'use strict';
+
+	var DocumentationRedirect = function () {
+	};
+
+	window.DocumentationRedirect = new DocumentationRedirect();
 })();
